7dad3096124066559e05ac70c6ae78c792f97253
PUCPR <pucpr@LADMAC0102.local>
2019-04-08 19:40:27 -0300

    Ëxmeplo de pods

---
+++ CapacitacaoIOS-Pods/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  CapacitacaoIOS-Pods
+//
+//  Created by PUCPR on 29/03/19.
+//  Copyright © 2019 PUCPR. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class ViewController: UIViewController {
+    @IBOutlet weak var lblTitulo: UILabel!
+    @IBOutlet weak var imvCapaFilme: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let urlBase = "https://api.themoviedb.org/3/movie/284052?api_key="
+        
+        let key = "650ce1911372d393d8ef3aeaf214ced3"
+        
+        let url = "\(urlBase)\(key)"
+        
+        let baseImage = "https://image.tmdb.org/t/p/w500"
+        
+        Alamofire.request(url).responseJSON { (response) in print(response.result.value!)
+            if (response.result.isSuccess) {
+                let json = JSON(response.result.value!)
+//                print(json["title"])
+//                print(json["production_companies"][0]["name"])
+                
+                self.lblTitulo.text = json["title"].stringValue
+                
+                //Montar a url da Image
+                let imagePath = "\(baseImage)\(json["poster_path"].stringValue)"
+                
+                //criar a URL
+                let urlImage = URL(string: imagePath)
+                
+                // Fazer o download da image
+                let bytes = try? Data(contentsOf: urlImage!)
+                
+                //Criar Imagem apartir dos dados lidos
+                let image = UIImage(data: bytes!)
+                
+                //Mostrar image na view
+                self.imvCapaFilme.image = image
+            }
+        }
+    }
+}
+
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'CapacitacaoIOS-Pods' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for CapacitacaoIOS-Pods
+    pod 'SwiftyJSON'
+    pod 'Alamofire'
+
+end
